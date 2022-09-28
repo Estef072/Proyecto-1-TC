4dc0ae4583ce78c84024eb8503b21a454d2e9b16
cayomol01 <jacmg01@gmail.com>
2022-09-28 15:58:23 -0600

    comments

--- Automata.py
+++ Automata.py
@@ -39,6 +39,10 @@ class Automata():
                     if afn.name in trans.keys():
                         trans[afn.name] = {value: [key.name, anterior]}
                         
+                    #Transiiciones
+                    #Trans = transiciones
+                    #trans[afn.name] = {nombre del estado actual: {simobolo: estado al que va}}
+                    
                     else:
                         trans[afn.name] = {value: key.name}
                 anterior = key.name
@@ -46,6 +50,44 @@ class Automata():
             visited.append(afn)
                 
         return trans
+    
+    def subconjuntos(self):
+        stack = [self.start]
+        visited = []
+        trans = {}
+        #
+        
+        #if simbolo =="epsilon":
+            #stack.append(key) (Se añade el estado al que lleva con la transicion epsilon)
+            #subconjuntos = [[transiciones epsilon del primer recorrido], [transiciones segundo recorrido]....]
+            #subconjuntos[0] = transiciones primer recorrido
+            #for subconjuntos[0] (recorrer todos los estados que se encuentran en esa lista)
+            #otro for para recorrer los estados de cada uno de los estados en subconjuntos[0]
+        while len(stack) != 0:
+            #afn -> estado actual en el que se está
+            afn = stack.pop()
+            
+            #Transiciones del estado actual
+            transiciones = afn.transitions.items()
+
+            for key, value in transiciones:
+                if afn not in visited:
+                    stack.append(key)
+                    if afn.name in trans.keys():
+                        trans[afn.name] = {value: [key.name, anterior]}
+                        
+                    #Transiiciones
+                    #Trans = transiciones
+                    #trans[afn.name] = {nombre del estado actual: {simobolo: estado al que va}}
+                    
+                    else:
+                        trans[afn.name] = {value: key.name}
+                anterior = key.name
+                    
+            visited.append(afn)
+                
+        return trans
+        pass
             
     
     
