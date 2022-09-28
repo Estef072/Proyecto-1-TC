8b8d3c6e649bb8b1c6f66d7c085bd0c77bf097af
TheJPMZ <monzon.jpmz@gmail.com>
2022-09-28 16:32:48 -0600

    ➕ Subconjuntos.py

---
+++ Subconjuntos.py
@@ -0,0 +1,24 @@
+
+
+AFN = Graph([0, 1], 3, True, False, 0, 2, transition_table={0: {0: [0,1], 1: [0]}, 1: {1: 2}})
+
+AFN.simular("100")
+
+AFD = Graph([0, 1], 5, initial_state=0, final_states=[3, 4],
+            transition_table={0: {0: 1, 1: 2}, 1: {0: 2, 1: 3}, 2: {0: 2, 1: 4}, 3: {0: 3, 1: 3}, 4: {0: 4, 1: 4}})
+
+print(AFD)
+AFD = minimizar(AFD)
+
+print(AFD)
+
+tta = Thompson(InfixPostfix("((ab)*)b"))
+tt = tta.Transiciones()
+ti = tta.start.name
+tf = tta.final.name
+regex = "aa+b*".replace("*","").replace("+","").replace("(","").replace(")","")
+talpha = list(set(list(regex)))
+tstates = list(tt.keys()) + list([tf] if tf not in tt else "")
+
+TEST = Graph(talpha,tstates,True,True,ti,[tf],tt)
+TEST.export("a")
--- main.py
+++ main.py
@@ -6,28 +6,57 @@ import time
 
 
 def main():
-    AFN = Graph([0, 1], 3, True, False, 0, 2, transition_table={0: {0: [0,1], 1: [0]}, 1: {1: 2}})
+    AFNDE = Graph(["a", "b"], 23, True, True, 6, [22], transition_table={0: {"#":[1,2]},
+                                                                     1: {"a": 3},
+                                                                     2: {"b": 4},
+                                                                     3: {"#": 5},
+                                                                     4: {"#": 5},
+                                                                     5: {"#": [0,7]},
+                                                                     6: {"#": [0,7]},
+                                                                     7: {"#": [8,15]},
+                                                                     8: {"a": 9},
+                                                                     9: {"b": 10},
+                                                                     10: {"b": 11},
+                                                                     11: {"#": [12,14]},
+                                                                     12: {"a": 13},
+                                                                     13: {"#": [12,14]},
+                                                                     14: {"#": 22},
+                                                                     15: {"#": [16,19]},
+                                                                     16: {"a": 17},
+                                                                     17: {"b": 18},
+                                                                     18: {"#": [16,19]},
+                                                                     19: {"b": 20},
+                                                                     20: {"a": 21},
+                                                                     21: {"#": 22},
+                                                                     })
 
-    AFN.simular("100")
+    def clausura(estado) -> list:
 
-    AFD = Graph([0, 1], 5, initial_state=0, final_states=[3, 4],
-                transition_table={0: {0: 1, 1: 2}, 1: {0: 2, 1: 3}, 2: {0: 2, 1: 4}, 3: {0: 3, 1: 3}, 4: {0: 4, 1: 4}})
+        lista = [estado]
+        marca = []
 
-    print(AFD)
-    AFD = minimizar(AFD)
+        while lista:
+            estado = lista.pop()
+            marca.append(estado) # Marcamos el estado
 
-    print(AFD)
+            try:
+                states = AFNDE.table[estado]['#']
+            except KeyError:
+                continue
 
-    tta = Thompson(InfixPostfix("((ab)*)b"))
-    tt = tta.Transiciones()
-    ti = tta.start.name
-    tf = tta.final.name
-    regex = "aa+b*".replace("*","").replace("+","").replace("(","").replace(")","")
-    talpha = list(set(list(regex)))
-    tstates = list(tt.keys()) + list([tf] if tf not in tt else "")
+            if not isinstance(states, list or tuple):
+                states = [states]
+            for x in states:
+                if x not in marca:
+                    lista.append(x)
+            lista = list(set(lista))
 
-    TEST = Graph(talpha,tstates,True,True,ti,[tf],tt)
-    TEST.export("a")
+        return list(set(marca))
+
+
+    mapa_claus = {}
+    for x in AFNDE.states:
+        mapa_claus.update({x:clausura(x)})
 
 
 if __name__ == '__main__':
